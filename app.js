76ed791824c054dcfab480b3cafe6d1c8cae288a
Obinnachieze <wisdomchieze0@gmail.com>
2026-01-01 15:41:57 +0100

    new

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-let submitBtn = document.getElementById("save-btn");
+const myLead = [];
+const inputEl = document.getElementById("input-el");
+const leadList = document.getElementById("lead-list");
+const submitBtn = document.getElementById("save-btn");
 
 submitBtn.addEventListener("click", function () {
-  console.log("Button clicked");
+  myLead.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLead.length; i++) {
+    listItems += `<li><a href='${myLead[i]}' target='_blank'>${myLead[i]}</a></li>`;
+  }
+  leadList.innerHTML = listItems;
+}
